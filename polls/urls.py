66ad74d1a5741e211a447fa64fb8135254f39340
Benoit Dherin <bdherin@berkeley.edu>
2014-03-09 13:45:01 -0700

    more changes

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
+
     url(r'^$', views.index, name='index'),
 
     # ex: /polls/5/
@@ -10,8 +11,11 @@ urlpatterns = patterns('',
 
     # ex: /polls/5/results/
     url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+
     # ex: /polls/5/vote/
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+
+    # ex: /polls/specifics/1/
     url(r'^specifics/(?P<poll_id>\d+)/$', views.detail, name='detail'),
 
 )
